f3c1ae0818f4687f5fd21df56aebad81fec83c11
GeunYoung Jang <zzbtang@gmail.com>
2022-07-26 21:56:32 +0900

    220726

--- nodejs/220722/index.js
+++ nodejs/220722/index.js
@@ -3,13 +3,19 @@ const app = express(); // app은 express 객체
 const port = 8080;
 
 app.set('view engine', 'ejs');
+
+// middleware를 사용할 때  use를 쓰는 건가?
 // app.use(express.static('public')); // 정적 파일들의 위치 => public
 app.use('/abc/aa', express.static('public')); //public에 접근하는 가상경로 : /abc , 정적 파일들의 위치 => public
+
+
 
 // // app.get('/', (req, res) => {
 // //     res.send('Hello Express!');
 // });
 
+// route, routing
+// 경로로 들어오면 콜백 함수 실행
 app.get('/', (req, res) =>{
     var list = ['apple', 'banana']
     // res.render('test');
--- nodejs/220725/index.js
+++ nodejs/220725/index.js
@@ -2,36 +2,71 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const bodyParser = require('body-parser');
+const { homedir } = require('os');
 const fs = require('fs').promises;
 
 app.set('view engine', 'ejs'); // set() 웹서버의 환경 설정
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true})); //{extended: true}를 하면 qs.parse를 사용하므로, object를 상속받을 수  있음
 app.use(bodyParser.json()); // json 형태로 받아옴.. 딕셔너리 형태..
 
 
 // 실습용
 app.get('/', function(req, res){
+    res.render('home');
+});
+app.get('/home', function(req, res){
+    res.render('home');
+});
+app.get('/signup', function(req, res){
+    res.render('signup');
+});
+app.get('/login', function(req, res){
     res.render('login');
 });
 
 app.post('/login_receive', function(req, res) {
-    res.render('login');
-});
+		let {id, pw} = req.body;
 
-// app.post('/signup_receive', function(req, res){
-//     console.log(req.body); // post일 때..
-//     console.log(req.query); // get일 때
-//     var id = req.body.id;
-//     var name = req.body.name;      
-//     var pw = req.body.pw;           // 구조분해로 해보자...
+		async function exec(){
+			//최초 로그인 시에만..
+			// await fs.writeFile('./signup/login_info.txt', `${id}//${pw}`);
+			let login_info = await (await fs.readFile('./signup/login_info.txt')).toString().split('//');
+			//console.log(login_info);
+			
+			if (id === login_info[0] && pw === login_info[1]) {
+				res.write("<script>alert('login success')</script>");
+			}
+			else {
+				res.write("<script>alert('login failure')</script>");
+			}};
+			
+		exec();
+		//res.render('login_receive', req.body)};
+	});
+			
+
+app.post('/signup_receive', function(req, res) {
+    console.log(req.body); // post일 때..
+    console.log(req.query); // get일 때
+		let {id, name, pw} = req.body;
+		async function exec(){
+			await fs.mkdir('./signup1');
+			await fs.writeFile('./signup1/info1.txt', `${id}//${name}//${pw}`);
+		};
+    res.redirect('home');
+	});
+
+//     // var id = req.body.id;
+//     // var name = req.body.name;      
+//     // var pw = req.body.pw;           // 구조분해로 해보자...
 
     
-//     async function exec(){
-//         await fs.mkdir('./signup');
-//         await fs.writeFile('./signup/info.txt', `${id}//${name}//${pw}`);
-//     }
-//     res.render('signup_receive', req.body);
+    // async function exec(){
+    //     await fs.mkdir('./signup1');
+    //     await fs.writeFile('./signup1/info.txt', `${id}//${name}//${pw}`);
+    // }
+    // res.render('signup_receive', req.body);
 //     exec();
 // });
 
