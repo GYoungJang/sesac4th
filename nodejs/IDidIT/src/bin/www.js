4c1c92d4aa85231d95c0fdca5ef6b9f76928994a
GeunYoung Jang <zzbtang@gmail.com>
2022-09-14 03:26:08 +0900

    220913

--- nodejs/IDidIT/src/bin/www.js
+++
@@ -1,6 +0,0 @@
-const app = require('../index');
-
-// 서버 띄우기
-app.listen(port, () => {
-  console.log('Server : ', port);
-});
--- nodejs/IDidIT/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctl = require('../controller/IDI_CTL');
-
-router.get('/', ctl.home);
-router.get('/signup', ctl.signup);
-router.get('/login', ctl.login);
