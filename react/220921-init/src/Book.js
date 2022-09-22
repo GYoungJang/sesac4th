643918138749f7eafd601362e7361c84c87e4211
GeunYoung Jang <zzbtang@gmail.com>
2022-09-23 03:16:01 +0900

    220923_react

--- react/220921-init/src/Book.js
+++ react/220921-init/src/Book.js
@@ -1,26 +1,29 @@
-import React, {Component} from 'react'
-import img from "./image.png";
+import React, { Component } from 'react';
+import img from './image.png';
 
 let style = {
-    backgroundColor : 'orange'
-
-}
+  backgroundColor: 'orange',
+};
 
 class Book extends Component {
-    render (){
-        console.log(this.props);
-        const {title, author, price, category} = this.props
-        return (
-            <div style={style}>
-                <h1>이번주 베스트셀러</h1>
-                <img src={img}></img><br></br>
-                <p>{title}</p>
-                <span>{author}</span><br></br>
-                <span>{price}</span><br></br>
-                <span>{category}</span><br></br>
-            </div>
-        ) 
-    }
+  render() {
+    console.log(this.props);
+    const { title, author, price, category } = this.props;
+    return (
+      <div style={style}>
+        <h1>이번주 베스트셀러</h1>
+        <img src={img} alt="책 이미지"></img>
+        <br></br>
+        <p>{title}</p>
+        <span>{author}</span>
+        <br></br>
+        <span>{price}</span>
+        <br></br>
+        <span>{category}</span>
+        <br></br>
+      </div>
+    );
+  }
 }
 
 export default Book;
