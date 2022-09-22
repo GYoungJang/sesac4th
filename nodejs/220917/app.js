df80823d8074138ac23327e89dd8beb084eb326e
GeunYoung Jang <zzbtang@gmail.com>
2022-09-22 19:40:13 +0900

    220922_chatting

--- nodejs/220917/app.js
+++ nodejs/220917/app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = express();
 const path = require('path');
 const server = http.createServer(app);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const socketIO = require('socket.io');
 const moment = require('moment');
 
