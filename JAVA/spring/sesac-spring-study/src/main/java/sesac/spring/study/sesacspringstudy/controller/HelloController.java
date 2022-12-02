6168e42a7ee28c7d7ed18ef0d53ecbc918229d47
user <user@userui-iMac.local>
2022-12-02 18:19:35 +0900

    221202_java

--- JAVA/spring/sesac-spring-study/src/main/java/sesac/spring/study/sesacspringstudy/controller/HelloController.java
+++ JAVA/spring/sesac-spring-study/src/main/java/sesac/spring/study/sesacspringstudy/controller/HelloController.java
@@ -2,10 +2,7 @@ package sesac.spring.study.sesacspringstudy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -61,4 +58,8 @@ public class HelloController {
         model.addAttribute("age", age);
         return "api";
     }
+
+    @GetMapping("dto")
+    @ResponseBody
+    public Person getDto()
 }
